f59338b1a8f410268066043bd974270e54f92828
theo <theo.kossoy@gmail.com>
2021-11-11 20:25:04 +0300

    fix: check

--- lab_6/check.go
+++ lab_6/check.go
@@ -7,43 +7,50 @@ import (
 	"strings"
 )
 
+func is_bst(array [][]int, index int, min int, max int) bool {
+	if index == 0 {
+		return true
+	}
+
+	index -= 1
+
+	if min <= array[index][0] && array[index][0] <= max {
+		return is_bst(array, array[index][1], min, array[index][0]-1) &&
+			is_bst(array, array[index][2], array[index][0]+1, max)
+	} else {
+		return false
+	}
+}
+
 func main() {
 	fin, _ := os.Open("check.in")
 	scanner := bufio.NewScanner(fin)
 	scanner.Split(bufio.ScanLines)
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
-	array := make([][]int, n)
 
-	for index := 0; index < n; index++ {
-		scanner.Scan()
-		elems := strings.Fields(scanner.Text())
-		arr := make([]int, 3)
-		for i := 0; i < 3; i++ {
-			arr[i], _ = strconv.Atoi(elems[i])
-		}
-		array[index] = arr
-	}
+	state := true
 
-	result := "YES"
-	for i := 0; i < n; i++ {
-		elem := array[i]
-		if elem[1] != 0 {
-			if array[elem[1]-1][0] > elem[0] {
-				result = "NO"
-				break
+	if n > 0 {
+		array := make([][]int, n)
+		for index := 0; index < n; index++ {
+			scanner.Scan()
+			elems := strings.Fields(scanner.Text())
+			arr := make([]int, 3)
+			for i := 0; i < 3; i++ {
+				arr[i], _ = strconv.Atoi(elems[i])
 			}
+			array[index] = arr
 		}
 
-		if elem[2] != 0 {
-			if array[elem[2]-1][0] < elem[0] {
-				result = "NO"
-				break
-			}
-		}
+		state = is_bst(array, 1, -1000000000, 1000000000)
 	}
 
 	fout, _ := os.Create("check.out")
-	fout.WriteString(result)
+	if state {
+		fout.WriteString("YES")
+	} else {
+		fout.WriteString("NO")
+	}
 	fout.Close()
 }
