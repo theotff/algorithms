8ec80c6af2da94f9d20c69ee100426a4cbce1f52
theo <theo.kossoy@gmail.com>
2022-01-15 16:03:44 +0300

    refactor: clean up digraph adj matrix

--- lab_8/adj_matrix.go
+++ lab_8/adj_matrix.go
@@ -9,45 +9,49 @@ import (
 	"strings"
 )
 
+type Graph struct {
+	verts     int
+	edges     int
+	adjMatrix [][]int
+}
+
 func main() {
 	fin, err := os.Open("input.txt")
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			fin, _ = os.Open("adj_matrix.in")
-		}
+	if errors.Is(err, os.ErrNotExist) {
+		fin, _ = os.Open("adj_matrix.in")
 	}
+	graph := &Graph{}
 	scanner := bufio.NewScanner(fin)
 	scanner.Split(bufio.ScanLines)
 	scanner.Scan()
 	info := strings.Fields(scanner.Text())
-	vertices, _ := strconv.Atoi(info[0])
-	edges, _ := strconv.Atoi(info[1])
 
-	adj_matrix := make([][]int, vertices)
-	for i := 0; i < vertices; i++ {
-		adj_matrix[i] = make([]int, vertices)
+	graph.verts, _ = strconv.Atoi(info[0])
+	graph.edges, _ = strconv.Atoi(info[1])
+
+	graph.adjMatrix = make([][]int, graph.verts)
+	for i := 0; i < graph.verts; i++ {
+		graph.adjMatrix[i] = make([]int, graph.verts)
 	}
 
-	for i := 0; i < edges; i++ {
+	for i := 0; i < graph.edges; i++ {
 		scanner.Scan()
-		points := strings.Fields(scanner.Text())
-		a, _ := strconv.Atoi(points[0])
-		b, _ := strconv.Atoi(points[1])
+		vertices := strings.Fields(scanner.Text())
+		a, _ := strconv.Atoi(vertices[0])
+		b, _ := strconv.Atoi(vertices[1])
 
-		adj_matrix[a-1][b-1] = 1
+		graph.adjMatrix[a-1][b-1] = 1
 	}
 
 	var fout *os.File
 	if err == nil {
 		fout, _ = os.Create("output.txt")
-	} else {
-		if errors.Is(err, os.ErrNotExist) {
-			fout, _ = os.Create("adj_matrix.out")
-		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		fout, _ = os.Create("adj_matrix.out")
 	}
 
-	for _, line := range adj_matrix {
-		arr := make([]string, vertices)
+	for _, line := range graph.adjMatrix {
+		arr := make([]string, graph.verts)
 		for index, elem := range line {
 			arr[index] = fmt.Sprint(elem)
 		}
