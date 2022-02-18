0231e4f209ba6e492bd416335b793fe142206a45
theo <theo.kossoy@gmail.com>
2022-02-19 00:04:35 +0300

    fix: topoSort

--- lab_09/topoSort.go
+++ lab_09/topoSort.go
@@ -35,48 +35,47 @@ type Graph struct {
 	verts   int
 	edges   int
 	adjList []*Node
-	visited []bool
-	cycle   []bool
+	visited []int
 }
 
 func (g *Graph) construct(verts, edges int) {
 	g.verts = verts
 	g.edges = edges
 	g.adjList = make([]*Node, g.verts)
-	g.visited = make([]bool, g.verts)
+	g.visited = make([]int, g.verts)
 }
 
 func (g *Graph) topoSort(sorted []int) bool {
 	s := &Stack{}
 	sortInd := 0
 	for index := range g.adjList {
-		if !g.visited[index] {
-			g.visited[index] = true
+		if g.visited[index] == 0 {
+			g.visited[index] = 1
 			s.push(index)
 			adj := g.adjList[index]
-			g.cycle = make([]bool, g.verts)
-			for adj != nil {
-				if !g.visited[adj.val] {
-					s.push(adj.val)
-					g.visited[adj.val] = true
-				}
-				if g.cycle[adj.val] {
-					return true
-				}
-				g.cycle[adj.val] = true
-				adj = g.adjList[adj.val]
-			}
+
 			for !s.isEmpty() {
 				ind := s.pop()
-				sorted[sortInd] = ind + 1
-				sortInd += 1
-				node := g.adjList[ind]
-				for node != nil {
-					if !g.visited[node.val] {
-						s.push(node.val)
-						g.visited[node.val] = true
+				adj = g.adjList[ind]
+				for adj != nil && g.visited[adj.val] != 0 {
+					adj = adj.next
+				}
+				if adj != nil {
+					s.push(ind)
+					for adj != nil {
+						switch g.visited[adj.val] {
+						case 0:
+							s.push(adj.val)
+							g.visited[adj.val] = 1
+						case 1:
+							return true
+						}
+						adj = g.adjList[adj.val]
 					}
-					node = node.next
+				} else {
+					g.visited[ind] = 2
+					sorted[sortInd] = ind
+					sortInd += 1
 				}
 			}
 		}
@@ -112,7 +111,7 @@ func main() {
 		fmt.Fprintln(fout, -1)
 	} else {
 		for i := len(sorted) - 1; i >= 0; i-- {
-			fmt.Fprint(fout, sorted[i], " ")
+			fmt.Fprint(fout, sorted[i]+1, " ")
 		}
 	}
 	fout.Close()
